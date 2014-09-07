553d9a6a49b627799f7af237262c86bc405d52fc
Tonodus <tonodus@freenet.de>
2014-09-07 16:31:18 +0200

    added openable blocks (fence gates, trapdoors) open/close blocks with right-click
    added warning if trying to open an non openable block
    
    added trapdoor to openable blocks
    
    removed auto-header, added javadocs
    
    fence gates should open themselves off from the player
    
    removed header

--- src/main/java/net/glowstone/block/ItemTable.java
+++ src/main/java/net/glowstone/block/ItemTable.java
@@ -87,6 +87,8 @@ public final class ItemTable {
         reg(Material.WEB, new BlockDirectDrops(Material.STRING));
         reg(Material.FIRE, new BlockFire());
         reg(Material.MONSTER_EGGS, new BlockDropless());
+        reg(Material.FENCE_GATE, new BlockFenceGate());
+        reg(Material.TRAP_DOOR, new BlockOpenable());
         reg(Material.FURNACE, new BlockFurnace());
         reg(Material.LEVER, new BlockLever());
         reg(Material.HOPPER, new BlockHopper());
