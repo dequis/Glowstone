f5fa78e8726b46a103a1c96e3722722a306be3e4
Tonodus <tonodus@freenet.de>
2014-09-20 20:26:36 +0200

    added ItemTool for easier damage handling; formatting; yield for explosions

--- src/main/java/net/glowstone/block/GlowBlock.java
+++ src/main/java/net/glowstone/block/GlowBlock.java
@@ -20,6 +20,7 @@ import org.bukkit.plugin.Plugin;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 /**
  * Represents a single block in a world.
@@ -302,19 +303,27 @@ public final class GlowBlock implements Block {
     ////////////////////////////////////////////////////////////////////////////
     // Drops and breaking
 
-    @Override
-    public boolean breakNaturally() {
+    public boolean breakNaturally(float yield) {
+        Random r = new Random();
+
         if (getType() == Material.AIR) {
             return false;
         }
 
         Location location = getLocation();
         for (ItemStack stack : getDrops()) {
-            getWorld().dropItemNaturally(location, stack);
+            if (r.nextFloat() < yield) {
+                getWorld().dropItemNaturally(location, stack);
+            }
         }
 
         setType(Material.AIR);
         return true;
+    }
+
+    @Override
+    public boolean breakNaturally() {
+        return breakNaturally(1.0f);
     }
 
     @Override
