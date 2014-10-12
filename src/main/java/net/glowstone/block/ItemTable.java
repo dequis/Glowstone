6ee727bbb97809636157bec6aa04e4bd845fa9ae
Tonodus <tonodus@freenet.de>
2014-10-12 13:26:10 +0200

    added BlockCactus

--- src/main/java/net/glowstone/block/ItemTable.java
+++ src/main/java/net/glowstone/block/ItemTable.java
@@ -132,6 +132,7 @@ public final class ItemTable {
         reg(Material.ENCHANTMENT_TABLE, new BlockEnchantmentTable());
         reg(Material.ANVIL, new BlockAnvil());
         reg(Material.BREWING_STAND, new BlockBrewingStand());
+        reg(Material.CACTUS, new BlockCactus());
 
         reg(Material.SIGN, new ItemSign());
         reg(Material.REDSTONE, new ItemPlaceAs(Material.REDSTONE_WIRE));
